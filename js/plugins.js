bb9b5fbfccb44950b0231f7b76203492f3a46a62
Dylan_Macnab <dylanmacnab@gmail.com>
2015-10-13 16:59:30 -0700

    built custom slider and slide content

--- js/plugins.js
+++ js/plugins.js
@@ -29,14 +29,3 @@
 
 // unslider
 // ----------------------------------------------------
-
-$(function() {
-    $('.plan__slider').unslider({
-      speed: 800,               //  The speed to animate each slide (in milliseconds)
-      delay: false,              //  The delay between slide animations (in milliseconds)
-      complete: function() {},  //  A function that gets called after every slide animation
-      keys: true,               //  Enable keyboard (left, right) arrow shortcuts
-      dots: true,               //  Display dot navigation
-      fluid: true              //  Support responsive design. May break non-responsive designs
-    });
-});
---
+++ js/slider.js
@@ -0,0 +1,49 @@
+// Custom Slider
+
+
+// Variables
+
+// Set index to 0
+var currentSlide = 0;
+// Store all slides
+var $slides = $('.slider .slide');
+// Store total number of slides
+var $totalSlides = $slides.length;
+
+
+// Functions
+// ----------------------------
+
+// Hides all slides, Displays Current index
+function cycleSlides() {
+  // stores current slide
+  var $slide = $('.slider .slide').eq(currentSlide);
+  // hides all slides
+  $slides.hide();
+  // display current slide
+  $slide.css('display', 'inline-block');
+}
+
+// call click function when next button is clicked
+$('.next').click(function() {
+  // update current slide index
+  currentSlide += 1;
+  // if on last slide, resent index to 0
+  if (currentSlide > $totalSlides - 1) {
+    currentSlide = 0;
+  }
+  // run cycle slides function to display slide
+  cycleSlides();
+});
+
+// call click function when prev button is clicked
+$('.prev').click(function() {
+  // update current slide index
+  currentSlide -= 1;
+  // if on first slide, move to last slide
+  if (currentSlide < 0) {
+    currentSlide = $totalSlides - 1;
+  }
+  // run cycle slides function to display slide
+  cycleSlides();
+});
