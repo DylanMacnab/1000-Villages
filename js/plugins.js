f23c66b6246fb89e72f9f000540f268eab847601
Dylan_Macnab <dylanmacnab@gmail.com>
2015-10-12 21:07:13 -0700

    completed through plan section

--- js/plugins.js
+++ js/plugins.js
@@ -25,3 +25,18 @@
 // Place any jQuery/helper plugins in here.
 // ---------------------------------------------------
 // ---------------------------------------------------
+
+
+// unslider
+// ----------------------------------------------------
+
+$(function() {
+    $('.plan__slider').unslider({
+      speed: 800,               //  The speed to animate each slide (in milliseconds)
+      delay: 5000,              //  The delay between slide animations (in milliseconds)
+      complete: function() {},  //  A function that gets called after every slide animation
+      keys: true,               //  Enable keyboard (left, right) arrow shortcuts
+      dots: true,               //  Display dot navigation
+      fluid: true              //  Support responsive design. May break non-responsive designs
+    });
+});
